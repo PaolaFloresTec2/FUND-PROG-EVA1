c9d088f404f076b3e786a5a875c46d582e3df72a
PaolaFloresTec2 <L26550284@chihuahua2.TECNM.MX>
2026-09-03 18:47:01 -0600

    Add files via upload

---
+++ EVA_2_HOLA_MUNDO/src/eva_2_hola_mundo/EVA_2_HOLA_MUNDO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_hola_mundo;
+
+/**
+ *
+ * @author paola
+ */
+public class EVA_2_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//INICIO DEL PROGRAMA
+        // TODO code application logic here
+        //ESTO ES UN BLOQUE DE CÓDIGO
+        //MOSTRAR UN MENSAJE DE TEXTO
+        //MOSTRAR --> INSTRUCCION
+        //TEXTO --> DATOS
+        System.out.println("Hola mundo!!");
+    }//FIN DEL PROGRAMA
+    
+}
