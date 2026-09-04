772bcfffafeae9398ba02b8b3af553ecf304388f
PaolaFloresTec2 <L26550284@chihuahua2.TECNM.MX>
2026-09-03 18:47:30 -0600

    Add files via upload

---
+++ EVA1_ 3_SALIDAS/src/eva1_/pkg3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_.pkg3_salidas;
+
+/**
+ *
+ * @author paola
+ */
+public class EVA1_3_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Fundamentos de Programación.");
+        System.out.println("Dana Paola Flores Mendoza");
+        System.out.println("Tu calificación es");
+        System.out.println(95);
+                            }
+    
+}
